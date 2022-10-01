74eeac24c20bd3452321762c0f29a7618ef0f2a4
Aakash Singh <92391710+Aakash231217@users.noreply.github.com>
2022-10-01 19:19:46 +0530

    Update djikstra.cpp

--- djikstra.cpp
+++ djikstra.cpp
@@ -6,56 +6,56 @@ using namespace std;
 void dijkstra(int G[max][max],int n, int startnode);
 int main()
 {
-int G[max][max]={{0,1,0,3,10},{1,0,5,0,0},{0,5,0,2,1},{3,0,2,0,6},{10,0,1,6,0}};
-int n=5,i,j;
-int u=0;
-dijkstra(G,n,u);
-return 0;
+      int G[max][max]={{0,1,0,3,10},{1,0,5,0,0},{0,5,0,2,1},{3,0,2,0,6},{10,0,1,6,0}};
+      int n=5,i,j;
+       int u=0;
+     dijkstra(G,n,u);
+     return 0;
 }
 void dijkstra(int G[max][max],int n,int startnode)
 {
-int cost[max][max], distance[max], pred[max];
-int visited[max], count, mindistance, nextnode, i, j;
-for(int i=0;i<n;i++)
-for(int j=0;j<n;j++)
-if(G[i][j]==0)
-cost[i][j]=INFINITY;
-else
-cost[i][j]=G[i][j];
-for(int i=0;i<n;i++) {
-distance[i]=cost[startnode][i];
-pred[i]=startnode;
-visited[i]=0;
-}
-distance[startnode]=0;
-visited[startnode]=1;
-count=1;
-while(count<n-1)
+       int cost[max][max], distance[max], pred[max];
+      int visited[max], count, mindistance, nextnode, i, j;
+       for(int i=0;i<n;i++)
+       for(int j=0;j<n;j++)
+      if(G[i][j]==0)
+       cost[i][j]=INFINITY;
+      else
+      cost[i][j]=G[i][j];
+        for(int i=0;i<n;i++) {
+      distance[i]=cost[startnode][i];
+      pred[i]=startnode;
+        visited[i]=0;
+      }
+        distance[startnode]=0;
+        visited[startnode]=1;
+          count=1;
+       while(count<n-1)
 {
-mindistance=INFINITY;
-for(int i=0;i<n;i++)
-if(distance[i]<mindistance && !visited[i]) {
-mindistance=distance[i];
-nextnode=i;
+        mindistance=INFINITY;
+       for(int i=0;i<n;i++)
+        if(distance[i]<mindistance && !visited[i]) {
+        mindistance=distance[i];
+          nextnode=i;
 }
-visited[nextnode]=1;
-for(int i=0;i<n;i++)
-if(!visited[i])
-if(mindistance+cost[nextnode][i]<distance[i]) {
-distance[i]=mindistance+cost[nextnode][i];
-pred[i]=nextnode;
+        visited[nextnode]=1;
+        for(int i=0;i<n;i++)
+        if(!visited[i])
+         if(mindistance+cost[nextnode][i]<distance[i]) {
+          distance[i]=mindistance+cost[nextnode][i];
+          pred[i]=nextnode;
 }
-count++;
+          count++;
 }
-for(int i=0;i<n;i++)
-if(i!=startnode)
-{
-cout<<"\nDistance of node"<<i<<"="<<distance[i];
-cout<<"\nPath="<<i;
-j=i;
-do {
-j=pred[j];
-cout<<"<-";
-}while(j!=startnode);
-}
+     for(int i=0;i<n;i++)
+      if(i!=startnode)
+   {
+       cout<<"\nDistance of node"<<i<<"="<<distance[i];
+      cout<<"\nPath="<<i;
+       j=i;
+     do {
+          j=pred[j];
+          cout<<"<-";
+     } while(j!=startnode);
+   }
 }
