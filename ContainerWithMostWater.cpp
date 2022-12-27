c3120dae236c8a4c775d71cb4d26217ab2625a7c
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2022-12-27 23:28:40 +0530

    GFG

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,13 @@
+long long ans =0,temp=0,i=0,j=len-1;
+    while(i<j){
+            int height=min(A[i],A[j]);
+            int breath=j-i;
+            temp=height*breath;
+            ans=max(ans,temp);
+    if(A[i]>A[j]){
+        --j;}
+    else {
+        ++i;
+        }
+    }
+    return ans;
