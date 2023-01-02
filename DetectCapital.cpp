4af24d5cf0c2218f7e8bda064595a8e0a7a397b5
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2023-01-02 22:53:35 +0530

    Leetcode

---
+++ DetectCapital.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        if(word.size()==1)return true;
+        for(int i=0;i<word.size();i++)
+        if(isupper(word[i]))count++;
+        if(count==1 && isupper(word[0]))return true;
+        else if(count==0 || count==word.size())return true;
+         return false;
+    }
+};
