91c74c78f58f63bdacfa5e048c96fb082312165e
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2023-01-10 00:20:08 +0530

    Leetcode

---
+++ PreorderTraversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> ans;
+    void preorder(TreeNode* node) {
+        if (node == NULL) return;
+        ans.push_back(node->val);
+        preorder(node->left);
+        preorder(node->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return ans;
+        
+    }
+};
