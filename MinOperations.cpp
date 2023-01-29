bafb9c5f01d06f8070b9be6e00c9beee9296a376
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2023-01-29 21:27:19 +0530

    GFG

---
+++ MinOperations.cpp
@@ -0,0 +1,13 @@
+ int solve(int a, int b) {
+        if(a==b)
+        return 0;
+        
+        if(a==0 || b==0)
+        return 1;
+        
+        int x = a&b;
+        if(a==x || b==x)
+        return 1;
+        
+        return 2;
+    }
