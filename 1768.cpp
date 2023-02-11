ee53b5f0deebd29b38157993e94e498f7b305a4c
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2023-02-11 23:17:54 +0530

    Leetcode

---
+++ 1768.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        
+        string res;
+        int i=0;
+        int j=0;
+        while(i<word1.size() or j<word2.size() ){
+            if(i<word1.size())res+=word1[i];
+            if(j<word2.size())res+=word2[j];
+            i++;
+            j++;
+        }
+        return res;
+    }
+};
