8ee31754ee2b702d8186a495a1cef00926cf3c47
Aakash Singh <92391710+Aakash231217@users.noreply.github.com>
2023-08-07 23:43:39 +0530

    Create 74.cpp

---
+++ 74.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool flag=false;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==target){
+                    return true; break;
+                }
+            }
+        }
+        return false;
+    }
+};
