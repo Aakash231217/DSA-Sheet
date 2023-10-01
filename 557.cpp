84e261474249da3302211b47a69203390c6c2786
Aakash Singh <92391710+Aakash231217@users.noreply.github.com>
2023-10-01 23:27:55 +0530

    Create 557.cpp

---
+++ 557.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream str(s);
+        string word="";
+        string ans="";
+        while(str>>word){
+            reverse(word.begin(),word.end());
+            ans+=word+" ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
